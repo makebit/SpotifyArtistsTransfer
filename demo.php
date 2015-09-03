490a3ea3af71f904c547609178f57ba6ca4cdf2d
makebit <send@makeb.it>
2015-09-03 11:10:49 +0200

    Initial Commit

--- demo.php
+++ demo.php
@@ -8,5 +8,5 @@
 
 include("SpotifyArtistsTransfer.php");
 
-$sat = new SpotifyArtistsTransfer("BQAYFON9OisrsI2U0Ub5OdbdEXI1llIq0GaRCWirXdwzYj6eeztBDSG6RcrW85OH3jsoN2acbSEgZ7y76_FoMtoQLJZo3hg6aLbkdDh-qlHAltcPCpSl_L8Atns05XIPOrfjpJOlj3XMbX52avWse1Po0pk","BQAYFON9OisrsI2U0Ub5OdbdEXI1llIq0GaRCWirXdwzYj6eeztBDSG6RcrW85OH3jsoN2acbSEgZ7y76_FoMtoQLJZo3hg6aLbkdDh-qlHAltcPCpSl_L8Atns05XIPOrfjpJOlj3XMbX52avWse1Po0pk");
+$sat = new SpotifyArtistsTransfer("tokenSender","tokenReceiver");
 echo $sat;
